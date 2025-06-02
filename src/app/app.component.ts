3020665515ae55cc780485f3850a0a5f6f97f412
Zanna-dev <pamn2704@gmail.com>
2025-06-02 17:31:22 +0100

    Implement Event Listener in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+    <app-header />
+    <main>
+      <app-home />
+    </main>
+    <!-- <app-user /> -->
+    
+    <router-outlet />
+    `,
+  styles: [ `
+    main{
+      padding: 16px;
+    }
+    `
+    
+  ],
 })
 export class AppComponent {
-  title = 'first-angular-project';
+  title = 'first-angular-projectv2';
 }
