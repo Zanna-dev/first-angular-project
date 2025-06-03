55e0f22b23fed36c546b4f64981a527daec416c2
Zanna-dev <pamn2704@gmail.com>
2025-06-03 01:22:03 +0100

    Create Routes in Angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { GreetingComponent } from '../componenets/greeting/greeting.component';
+import { CounterComponent } from '../componenets/counter/counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent],
+  imports: [GreetingComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
